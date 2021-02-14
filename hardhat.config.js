6c7492c5446451f45a845803b153f4bc30f3595c
GreenHornet <HoneyIsMoney@protonmail.com>
2021-02-14 14:48:23 +0300

    initial spike

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+
+const secret = require('./keys.json')
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    arbitum: {
+      url: "https://kovan3.arbitrum.io/rpc",
+      accounts: [secret.deployer]
+    }
+  }
+};
+
